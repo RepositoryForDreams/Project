273449eab5175810a6742cbac45d6ac1d24c6c06
ParkJeongHee <jgengine@naver.com>
2020-11-25 22:28:03 +0900

    [Merge] 크래쉬 부분 수정

--- ParkJeongHee/JGEngine/Source/UnitTest/ConsoleTest/main.cpp
+++ ParkJeongHee/JGEngine/Source/UnitTest/ConsoleTest/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
-
+#include <crtdbg.h>
 #include "JGMemory.h"
 #include "JGMath.h"
-#pragma comment(lib, "../Bin/lib/Debug/JGCore.lib")
-
+// #pragma comment(lib, "../Bin/lib/Debug/JGCore.lib")
+#pragma comment(lib, "../Bin/lib/Release/JGCore.lib")
 using namespace std;
 using namespace JG;
 struct test
@@ -17,21 +17,21 @@ struct BlockHeader
 {
 	ptraddr prev;         // 전 블록의 주솟값
 	ptraddr next;         // 다음 블록의 주솟값
+	ptraddr handle;
 	u64     blockSize;    // 블록 사이즈(전체)
-	bool    used = false; // 사용 했나 안했나
-	byte    padding[7];   //
+
 };
-//void blockHeapAllocatePrint(void* ptr)
-//{
-//	BlockHeader* bh = (BlockHeader*)ptr;
-//
-//	printf("시작 주소 : %llu \n", bh);
-//	printf("할당 여부 : %d \n", bh->used);
-//	printf("메모리 크기 : %llu \n", bh->blockSize);
-//	printf("이전 주소 : %llu \n", bh->prev);
-//	printf("다음 주소 : %llu \n", bh->next);
-//	printf("\n\n");
-//}
+void blockHeapAllocatePrint(void* ptr)
+{
+	BlockHeader* bh = (BlockHeader*)ptr;
+
+	printf("시작 주소 : %llu \n", (ptraddr)bh);
+	printf("메모리 핸들 : %llu \n", bh->handle);
+	printf("메모리 크기 : %llu \n", bh->blockSize);
+	printf("이전 주소 : %llu \n", bh->prev);
+	printf("다음 주소 : %llu \n", bh->next);
+	printf("\n\n");
+}
 //void blockStackAllocatePrint(string name, void* ptr, u64 size)
 //{
 //	u64* blockSize = (u64*)((u64)ptr - sizeof(u64));
@@ -73,11 +73,11 @@ struct BlockHeader
 //	printf("\n\n");
 //}
 //
-//void HeapAllocatorInfoPrint(JG::JGAllocatorManager& manager)
+//void HeapAllocatorInfoPrint()
 //{
 //
 //	printf("HeapAllocator 정보 \n");
-//	void* start = manager.GetHeapAllocator().GetStartPtr();
+//	void* start = (void*)JG::JGAllocatorManager::GetInstance()->mHeapAllocator.GetStartPtr();
 //	BlockHeader* bh = (BlockHeader*)start;
 //	while (true)
 //	{
@@ -92,6 +92,28 @@ struct BlockHeader
 //
 //}
 
+#include <chrono>
+
+
+
+class TestObject : public JGObject
+{
+public:
+	TestObject() {
+	
+	}
+	virtual ~TestObject() {
+	
+	}
+};
+
+class TT
+{
+public:
+	TT() {}
+	~TT() {}
+};
+
 
 void blockDeAllocatePrint(string name, void* ptr)
 {
@@ -100,144 +122,51 @@ void blockDeAllocatePrint(string name, void* ptr)
 
 int main()
 {
+	// 문제점 발견
 	JGAllocatorDesc desc;
-	desc.stackAllocMem = 256;
-	desc.linearAllocMem = 256;
-	desc.heapAllocMem = 1024;
-	desc.singleFrameAllocMem = 128;
-	desc.doubleBufferedAllocMem = 256;
+	desc.StackAllocMem = _MB;
+	desc.LinearAllocMem = _KB;
+	desc.HeapAllocMem = _MB;
+	desc.SingleFrameAllocMem = 128;
+	desc.DoubleBufferedAllocMem = 256;
+	desc.MemoryDefragmenterCountPerFrame = 20;
+	JG::JGAllocatorManager::Create(desc);
 
-	JG::JGAllocatorManager AllocManager(desc);
+	{
+		std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
 
-
-	//// stack 할당
-	//{
-	//	StackAllocatorInfoPrint(AllocManager);
-
-	//	void* p1 = (test*)AllocManager.GetStackAllocator().Alloc(sizeof(test));
-	//
-	//	blockStackAllocatePrint("p1", p1, sizeof(test));
-
-	//	void* p2 = (test*)AllocManager.GetStackAllocator().Alloc(sizeof(test) * 10);
-
-	//	blockStackAllocatePrint("p2", p2, sizeof(test) * 10);
-
-	//	void* p3 = (test*)AllocManager.GetStackAllocator().Alloc(sizeof(test) * 4);
-
-	//	blockStackAllocatePrint("p3", p3, sizeof(test) * 4);
+		std::unique_ptr<TestObject> testNew[10000];
+		for (int i = 0; i < 10000; ++i)
+		{
+			testNew[i] = std::make_unique<TestObject>();
+		}
 
 
 
-	//	StackAllocatorInfoPrint(AllocManager);
+		std::chrono::duration<double> sec = std::chrono::system_clock::now() - start;
+
+		cout << "new 10000번 메모리 할당 : " << sec.count() << endl;
+
+		start = std::chrono::system_clock::now();
+
+		JGUniquePtr<TestObject> testNew2[10000];
+		for (int i = 0; i < 10000; ++i)
+		{
+			testNew2[i] = CreateUniquePtr<TestObject>();
+		}
+
+		sec = std::chrono::system_clock::now() - start;
+
+		cout << "custom 10000번 메모리 할당 : " << sec.count() << endl;
+
+	}
+
+	
 
 
-	//	AllocManager.GetStackAllocator().DeAlloc(&p3);
+	JG::JGAllocatorManager::Destroy();
 
-	//	StackAllocatorInfoPrint(AllocManager);
-
-	//	AllocManager.GetStackAllocator().DeAlloc(&p2);
-
-	//	StackAllocatorInfoPrint(AllocManager);
-
-	//	AllocManager.GetStackAllocator().DeAlloc(&p1);
-
-	//	StackAllocatorInfoPrint(AllocManager);
-
-	//}
-
-	//// Linear 할당
-	//{
-
-	//	LinearAllocatorInfoPrint(AllocManager);
-
-	//	void* p1 = (test*)AllocManager.GetLinearAllocator().Alloc(sizeof(test));
-
-	//	blockLinearAllocatePrint("p1", p1, sizeof(test));
-	//	LinearAllocatorInfoPrint(AllocManager);
-
-
-
-	//	void* p2 = (test*)AllocManager.GetLinearAllocator().Alloc(sizeof(test) * 10);
-	//	blockLinearAllocatePrint("p2", p2, sizeof(test) * 10);
-	//	LinearAllocatorInfoPrint(AllocManager);
-
-
-
-	//	void* p3 = (test*)AllocManager.GetLinearAllocator().Alloc(sizeof(test) * 4);
-
-	//	blockLinearAllocatePrint("p3", p3, sizeof(test) * 4);
-	//	LinearAllocatorInfoPrint(AllocManager);
-	//}
-
-	//// Heap 할당
-	//{
-	//	void* p1 = (test*)AllocManager.GetHeapAllocator().Alloc(sizeof(test));
-	//	void* p2 = (test*)AllocManager.GetHeapAllocator().Alloc(sizeof(test) * 4);
-	//	void* p3 = (test*)AllocManager.GetHeapAllocator().Alloc(sizeof(test) * 10);
-	//	void* p4 = (test*)AllocManager.GetHeapAllocator().Alloc(sizeof(test) * 3);
-
-	//	HeapAllocatorInfoPrint(AllocManager);
-
-	//	cout << "할당 해제" << endl << endl;
-	//	AllocManager.GetHeapAllocator().DeAlloc(&p2);
-	//	AllocManager.GetHeapAllocator().DeAlloc(&p4);
-	//	AllocManager.GetHeapAllocator().DeAlloc(&p1);
-	//	HeapAllocatorInfoPrint(AllocManager);
-
-
-
-	//	cout << "다시 할당" << endl << endl;
-	//	p2 = (test*)AllocManager.GetHeapAllocator().Alloc(sizeof(test) * 30);
-	//	p4 = (test*)AllocManager.GetHeapAllocator().Alloc(sizeof(test) * 3);
-	//	void* p5 = (test*)AllocManager.GetHeapAllocator().Alloc(sizeof(test) * 17);
-	//	void* p6 = (test*)AllocManager.GetHeapAllocator().Alloc(sizeof(test) * 3);
-	//	HeapAllocatorInfoPrint(AllocManager);
-
-
-	//	cout << "할당 해제" << endl << endl;
-	//	AllocManager.GetHeapAllocator().DeAlloc(&p2);
-	//	AllocManager.GetHeapAllocator().DeAlloc(&p5);
-	//	
-	//	HeapAllocatorInfoPrint(AllocManager);
-
-	//}
-
-	//// SingleAllocator 할당
-	//{
-	//	test* p1 = (test*)AllocManager.GetSingleFrameAllocator().Alloc(sizeof(test));
-	//	test* p2 = (test*)AllocManager.GetSingleFrameAllocator().Alloc(sizeof(test) * 3);
-	//	p1->a = 3;
-	//	p1->b = 6;
-	//	AllocManager.GetSingleFrameAllocator().Clear();
-
-	//	test* p3 = (test*)AllocManager.GetSingleFrameAllocator().Alloc(sizeof(test));
-	//	p3->a = 1;
-	//	p3->b = 2;
-	//	p1 = nullptr;
-	//	p2 = nullptr;
-
-	//}
-	//{
-	//	test* p1 = (test*)AllocManager.GetDoubleFrameAllocator().Alloc(sizeof(test));
-	//	test* p2 = (test*)AllocManager.GetDoubleFrameAllocator().Alloc(sizeof(test) * 3);
-	//	p1->a = 2;
-	//	
-	//	AllocManager.GetDoubleFrameAllocator().Swap();
-
-	//	test* p3 = (test*)AllocManager.GetDoubleFrameAllocator().Alloc(sizeof(test));
-	//	p3->a = 2;
-	//	test* p4 = (test*)AllocManager.GetDoubleFrameAllocator().Alloc(sizeof(test) * 3);
-
-	//	AllocManager.GetDoubleFrameAllocator().Swap();
-
-	//	test* p5 = (test*)AllocManager.GetDoubleFrameAllocator().Alloc(sizeof(test));
-	//	p5->a = 0;
-	//	test* p6 = (test*)AllocManager.GetDoubleFrameAllocator().Alloc(sizeof(test) * 3);
-	//}
-
-
-
-
+	//_CrtDumpMemoryLeaks();
 	return 0;
 }
 
